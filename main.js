824a4fbd51af2cdee35db85cae3639cee6d8452e
orgewap <orgewap@gmail.com>
2025-03-31 21:45:14 +0800

    update index and main, completed

--- main.js
+++ main.js
@@ -23,6 +23,41 @@ async function process() {
     
         select.add(option) // добавляем элемент к выпадающему списку
     })
+
+    mappedData = data.map(item => {
+        return item['Насколько доволен форматом обучения?'];
+    })
+
+    uniqueValues = [...new Set(mappedData)]
+
+    select = document.querySelector("#selectDovolnost");
+
+    uniqueValues.forEach(item => {
+        let option = document.createElement("option"); 
+
+        option.value = item; 
+        option.text = item;
+
+        select.add(option) 
+    })
+
+    mappedData = data.map(item => {
+        return item['Отметь, в какой мере ты удовлетворен курсом?'];
+    })
+
+    uniqueValues = [...new Set(mappedData)]
+
+    select = document.querySelector("#selectUdovletvoronost");
+
+    uniqueValues.forEach(item => {
+        let option = document.createElement("option"); 
+
+        option.value = item; 
+        option.text = item;
+
+        select.add(option) 
+    })
+
 }
 
 process()
